28a99e6f6e5e49ef554821caf261abd66fe002bb
Mike Rossetti <s2t2@users.noreply.github.com>
2019-03-13 16:42:35 -0400

    Update code to send a tweet

--- app/send_tweet.py
+++ app/send_tweet.py
@@ -1,6 +1,10 @@
-# adapted from: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/packages/tweepy.md
+# adapted from:
+# ... https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/packages/tweepy.md
+# ... https://www.geeksforgeeks.org/tweet-using-python/
 
+import datetime
 import os
+import pprint
 
 from dotenv import load_dotenv
 import tweepy
@@ -21,18 +25,24 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 client = tweepy.API(auth)
 
-# ISSUE REQUESTS
+# ISSUE REQUEST(S)
 
 user = client.me() # get information about the currently authenticated user
 
-tweets = client.user_timeline() # get a list of tweets posted by the currently authenticated user
+time_now = datetime.datetime.now() # a way for us to implement status uniqueness to avoid subsequent tweets running into ... tweepy.error.TweepError: [{'code': 187, 'message': 'Status is a duplicate.'}]
+time_now_formatted = str(time_now) #> '2019-03-13 16:41:26.282159'
+status = f"An example tweet sent programmatically from https://github.com/prof-rossetti/notification-service-py at {time_now_formatted}"
+response = client.update_status(status=status)
 
 # PARSE RESPONSES
 
-print("---------------------------------------------------------------")
-print(f"RECENT TWEETS BY @{user.screen_name} ({user.followers_count} FOLLOWERS / {user.friends_count} FOLLOWING):")
-print("---------------------------------------------------------------")
+pp = pprint.PrettyPrinter(indent=4)
 
-for tweet in tweets:
-    created_on = tweet.created_at.strftime("%Y-%m-%d")
-    print(" + ", tweet.id_str, created_on, tweet.text)
+print("---------------------------------------------------------------")
+print(f"TWEETING AS... @{user.screen_name} ({user.followers_count} FOLLOWERS / {user.friends_count} FOLLOWING)")
+print("---------------------------------------------------------------")
+print("RESPONSE:", type(response))
+print("ID:", response.id_str)
+print("TEXT:", response.text)
+print("PROPERTIES:")
+pp.pprint(dict(response._json))
