20d4aba82f343c49f8a3dc8c61b803801b907346
Mike Rossetti <s2t2@users.noreply.github.com>
2020-05-28 15:39:46 -0400

    Upgrade sendgrid; closes #5

--- app/send_email.py
+++ app/send_email.py
@@ -5,41 +5,34 @@ import os
 import pprint
 
 from dotenv import load_dotenv
-import sendgrid
-from sendgrid.helpers.mail import * # source of Email, Content, Mail, etc.
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 load_dotenv()
 
 SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
-MY_EMAIL_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
+MY_ADDRESS = os.environ.get("MY_EMAIL_ADDRESS", "OOPS, please set env var called 'MY_EMAIL_ADDRESS'")
 
 # AUTHENTICATE
 
-sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
+client = SendGridAPIClient(SENDGRID_API_KEY)
+print("CLIENT:", type(client))
 
 # COMPILE REQUEST PARAMETERS (PREPARE THE EMAIL)
 
-from_email = Email(MY_EMAIL_ADDRESS)
-to_email = Email(MY_EMAIL_ADDRESS)
 subject = "Example Notification"
-message_text = "Hello, \n\nThis is a message from your personal notification service.\n\nCustomize this example notification content to make it useful for you! Maybe weather info? Maybe stock prices? Let your creativity guide you!"
-content = Content("text/plain", message_text)
-mail = Mail(from_email, subject, to_email, content)
+content = "Hello, \n\nThis is a message from your personal notification service.\n\nCustomize this example notification content to make it useful for you! Maybe weather info? Maybe stock prices? Let your creativity guide you!"
+message = Mail(from_email=MY_ADDRESS, to_emails=MY_ADDRESS, subject=subject, html_content=content)
+print("MESSAGE:", type(message))
 
 # ISSUE REQUEST (SEND EMAIL)
 
-response = sg.client.mail.send.post(request_body=mail.get())
+try:
+    response = client.send(message)
+    print("RESPONSE:", type(response))
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
 
-# PARSE RESPONSE
-
-pp = pprint.PrettyPrinter(indent=4)
-
-print("----------------------")
-print("EMAIL")
-print("----------------------")
-print("RESPONSE: ", type(response))
-print("STATUS:", response.status_code) #> 202 means success
-print("HEADERS:")
-pp.pprint(dict(response.headers))
-print("BODY:")
-print(response.body) #> this might be empty. it's ok.)
+except Exception as e:
+    print("OOPS", e)
